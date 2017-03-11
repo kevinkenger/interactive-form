c2d39ffb7729907a2479886631f5a30ded95c1bd
Kevin Kenger <kevinkenger@gmail.com>
2017-03-11 12:30:32 -0700

    add tshirt picker functionality

--- js/app.js
+++ js/app.js
@@ -1,7 +1,38 @@
 const nameInput = document.getElementById('name'),
-      jobTitle = document.getElementById('other-title');
+      jobTitle = document.getElementById('other-title'),
+      basicInfo = document.getElementsByTagName('fieldset')[0],
+      tshirtInfo = document.getElementsByClassName('shirt')[0],
+      activitiesInfo = document.getElementsByClassName('activities')[0],
+      paymentInfo = document.getElementsByTagName('fieldset')[0];
 
 window.onload = () => {    
     nameInput.focus();
     jobTitle.style.display = 'none';
 }
+
+tshirtInfo.addEventListener('change', (e) => {
+    if ( e.target.id != 'design' ) {
+        return;
+    } else {
+        const colors = document.getElementById('color').children;
+        if ( e.target.value == 'js puns' ) {
+            for ( let i = 0; i < colors.length; i++ ) {
+                if ( i < 3 ) {
+                    colors[i].style.display = '';
+                } else {
+                    colors[i].style.display = 'none';
+                }
+            }
+            colors[0].selected = true;
+        } else {
+            for ( let i = 0; i < colors.length; i++ ) {
+                if ( i < 3 ) {
+                    colors[i].style.display = 'none';
+                } else {
+                    colors[i].style.display = '';
+                }
+            }
+            colors[3].selected = true;
+        }
+    }
+});

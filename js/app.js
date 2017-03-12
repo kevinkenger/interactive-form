5c6c4f7e6017604643edc5a57c5bf256aac0750a
Kevin Kenger <kevinkenger@gmail.com>
2017-03-12 15:26:38 -0700

    added comments

--- js/app.js
+++ js/app.js
@@ -7,22 +7,24 @@ const totalEl = document.createElement('div'),
       tshirtInfo = document.getElementsByClassName('shirt')[0],
       paymentInfo = document.getElementsByTagName('fieldset')[3],
       activitiesInfo = document.getElementsByClassName('activities')[0];
-      
-let total = 0,
-    errors = 0;
 
-totalEl.textContent = `Total: $${total}`;
 
+let total = 0, // set starting total sum at zero
+    activityChosen; // boolean value to check if an activity has been chosen
+
+totalEl.textContent = `Total: $${total}`; // add indicator text to total element
+
+// On page load...
 window.onload = () => {    
-    nameInput.focus();
-    jobTitle.style.display = 'none';
-    totalEl.id = 'total';
-    activitiesInfo.appendChild(totalEl);
-    tshirtInfo.children[4].style.display = 'none';
+    nameInput.focus(); // set cursor in name input
+    jobTitle.style.display = 'none'; // hide the #other-title element until needed
+    totalEl.id = 'total'; // give the total indicator element the id of "#total"
+    activitiesInfo.appendChild(totalEl); // add "#total" to the activites section
+    tshirtInfo.children[4].style.display = 'none'; // hide the colors select field
 }
 
 
-
+// SELECTING JOB TITLE
 basicInfo.addEventListener('change', (e) => {
     if ( e.target.value == 'other' ) {
         jobTitle.style.display = '';
@@ -33,13 +35,13 @@ basicInfo.addEventListener('change', (e) => {
 });
 
 
-
-
+// SELECTING T-SHIRT
 tshirtInfo.addEventListener('change', (e) => {
+    // if the element that triggered the change isn't "design"...
     if ( e.target.id != 'design' ) {
-        return;
+        return; // exit the function
     }
-    tshirtInfo.children[4].style.display = '';
+    tshirtInfo.children[4].style.display = ''; // show the color options select
     const colors = document.getElementById('color').children;
     if ( e.target.value == 'js puns' ) {
         for ( let i = 0; i < colors.length; i++ ) {
@@ -63,10 +65,10 @@ tshirtInfo.addEventListener('change', (e) => {
 });
 
 
-
-
+// CHOOSING AN ACTIVITY
 activitiesInfo.addEventListener('change', (e) => {
     const checkbox = e.target,
+    // create arrays of activities depending on their time
           time1 = [
             document.getElementsByName('js-frameworks')[0],
             document.getElementsByName('express')[0],
@@ -77,46 +79,54 @@ activitiesInfo.addEventListener('change', (e) => {
               document.getElementsByName('node')[0],
               document.getElementsByName('npm')[0]
           ];
-    if ( time1.indexOf(checkbox) > -1 ) {
-        activitiesChecker(checkbox, time1);
-    } else if ( time2.indexOf(checkbox) > -1 ) {
-        activitiesChecker(checkbox, time2);
+
+    // if checkbox is checked...
+    if ( checkbox.checked ) {
+        // indicate an activity has been chosen
+        activityChosen = true;
+    // if checkbox isn't checked...
     } else {
+        // reset activityChosen to false in case no other activites are selected
+        activityChosen = false;
+        // iterate through each of the elements in the activites section
+        for ( let i = 0, a = activitiesInfo.children; i < a.length; i++ ) {
+            // if the current element is a label (parent of checkbox)...
+            if ( a[i].tagName == 'LABEL' ) {
+                // if the checkbox is checked...
+                if ( a[i].firstElementChild.checked ) {
+                    // indicate an activity has been chosen
+                    activityChosen = true;
+                }
+            }
+        }
+    }
+
+    // if the checkbox that was cliked is in the first array...
+    if ( time1.indexOf(checkbox) > -1 ) {
+        // send the checked checkbox and the array it's in to the activitiesChecker function
+        activitiesChecker(checkbox, time1);  
+    // else if the checkbox that was clicked is in the second array...
+    } else if ( time2.indexOf(checkbox) > -1 ) {
+        // send the checked checkbox and the array it's in to the activitiesChecker function
+        activitiesChecker(checkbox, time2);
+    // else, it's the "main conference" checkbox...
+    } else {
+        // so just send the to the activitiesChecker function
         activitiesChecker(checkbox);
     }
 });
 
-const activitiesChecker = (checkbox, time) => {
-    if ( time ) {
-        for ( let i = 0; i < 3; i++ ) {
-            if ( time[i] != checkbox ) {
-                time[i].disabled = checkbox.checked;
-            }
-        }
-    }
-    if ( checkbox.checked ) {
-        if ( checkbox.name == 'all' ) {
-            total += 200;
-        } else {
-            total += 100;
-        }
-    } else {
-        if ( checkbox.name == 'all' ) {
-            total -= 200;
-        } else {
-            total -= 100;
-        }
-    }
-    totalEl.textContent = `Total: $${total}`;
-}
 
-
+// SELECTING PAYMENT METHOD
 paymentInfo.addEventListener('change', (e) => {
+    // if the element that triggered the change isn't "payment"...
     if ( e.target.id != 'payment' ) {
-        return;
+        return; // exit the function
     }
+
     const payment = e.target,
           cc = document.getElementById('credit-card');
+    // check to see which payment type the user chooses and display the corresponding element
     switch ( payment.value ) {
         case 'credit card':
             cc.style.display = '';
@@ -142,6 +152,7 @@ paymentInfo.addEventListener('change', (e) => {
 });
 
 
+// MINOR CREDIT CARD VALIDATION
 paymentInfo.addEventListener('keyup', (e) => {
     if ( e.target.id == 'cvv' || e.target.id == 'zip' || e.target.id == 'cc-num') {
         if ( e.code.match(/Key/) ) {
@@ -156,84 +167,152 @@ paymentInfo.addEventListener('keyup', (e) => {
 });
 
 
-
-const errorOnField = (input, bool) => {
-    const label = input.previousElementSibling;
-    if ( bool ) {
-        label.style.color = 'darkred';
-        label.style.fontWeight = 'bold';
-        label.firstElementChild.style.display = 'inline';
-    } else {
-        label.style.color = 'black';
-        label.style.fontWeight = 'normal';
-        label.firstElementChild.style.display = 'none';
-    }
-}
-
-function validateEmail(email) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-}
-
-function validateCreditCard(cc) {
-    var re = /^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][‌​0-9])[0-9]{12}|3[47]‌​[0-9]{13}|3(?:0[0-5]‌​|[68][0-9])[0-9]{11}‌​|(?:2131|1800|35\\d{‌​3})\\d{11})$/
-    return re.test(cc);
-}
-
+// SUBMITTING THE FORM
 form.addEventListener('submit', (e) => {
+    // prevent the form from actually being submitted
     e.preventDefault();
 
-    let activityChosen,
-        paymentMethodChosen;
 
+    // if the name input is empty...
     if ( nameInput.value.trim() == '' ) {
+        // display the error
         errorOnField(nameInput, true);
+    // if the name input is not empty...
     } else {
+        // remove the error
         errorOnField(nameInput, false);
     }
+    // if email input is emtpy or not valid...
     if ( emailInput.value.trim() == '' || !validateEmail(emailInput.value) ) {
+        // display the error
         errorOnField(emailInput, true);
+    // if email input is not empty and is valid...
     } else {
+        // remove the error
         errorOnField(emailInput, false);
     }
 
+    // if the user hasn't chosen a shirt design...
     if ( document.getElementById('design').value != 'js puns' && document.getElementById('design') != 'heart js' ) {
+        // show the error
         tshirtInfo.children[1].style.display = 'block';
+    // if the user has chosen a design...
     } else {
+        // remove the error
         tshirtInfo.children[1].style.display = 'none';
     }
 
-    for ( let i = 0; i < activitiesInfo.children.length; i++ ) {
-        if ( activitiesInfo.children[i].tagName == 'LABEL' ) {
-            if ( activitiesInfo.children[i].firstElementChild.checked ) {
-                activityChosen = true;
-            }
-        }
-    }
-
+    // if an activity has been chosen
     if ( activityChosen ) {
         activitiesInfo.children[1].style.display = 'none';
     } else {
         activitiesInfo.children[1].style.display = 'block';
     }
 
+    // if a payment method hasn't been chosen...
     if ( document.getElementById('payment').value == 'select_method' ) {
+        // display the error
         paymentInfo.children[1].style.display = 'block';
+    // if a payment method has been choosen...
     } else {
+        // remove the error
         paymentInfo.children[1].style.display = 'none';
+
+        // if the user selects credit card...
         if ( document.getElementById('payment').value == 'credit card' ) {
-            if ( document.getElementById('cc-num').value == '' || document.getElementById('zip').value == '' || document.getElementById('cvv').value == '' ) {
+            // if any of the credit card input fields are empty...
+            if ( document.getElementById('cc-num').value.trim() == '' || document.getElementById('zip').value.trim() == '' || document.getElementById('cvv').value.trim() == '' ) {
+                // display the error
                 document.getElementById('credit-card').firstElementChild.style.display = 'block';
+            // if all of the credit card input fields are filled out...    
             } else {
+                // remove the error
                 document.getElementById('credit-card').firstElementChild.style.display = 'none';
             }
+            // if the credit card number isn't valid...
             if ( !validateCreditCard(document.getElementById('cc-num').value) ) {
+                // display the error
                 document.getElementById('credit-card').children[1].style.display = 'block';
+            // if the credit number is valid
             } else {
+                // remove the error
                 document.getElementById('credit-card').children[1].style.display = 'none';
             }
         }
     }
-
-
 });
+
+
+// Error handling for text inputs
+const errorOnField = (input, bool) => { // takes input element and true/false to determine whether to display the error
+    const label = input.previousElementSibling;
+    // if bool is true...
+    if ( bool ) {
+        // style the label and display the error
+        label.style.color = 'darkred';
+        label.style.fontWeight = 'bold';
+        label.firstElementChild.style.display = 'inline';
+    // if bool is false...
+    } else {
+        // style the label normally and remove the error
+        label.style.color = 'black';
+        label.style.fontWeight = 'normal';
+        label.firstElementChild.style.display = 'none';
+    }
+}
+
+// Validating the email address
+const validateEmail = (email) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    // call .test() (RegExp prototype method) to check whether the entered email is formatted correctly
+    return re.test(email);
+}
+
+const validateCreditCard = (cc) => {
+    const re = /^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][‌​0-9])[0-9]{12}|3[47]‌​[0-9]{13}|3(?:0[0-5]‌​|[68][0-9])[0-9]{11}‌​|(?:2131|1800|35\\d{‌​3})\\d{11})$/
+    // call .test() (RegExp prototype method) to check whether the entered cc number is formatted correctly
+    return re.test(cc);
+}
+
+
+// accepts two args: the checkbox that was clicked and the array it's in
+const activitiesChecker = (checkbox, time) => {
+    // if the checkbox isn't the "main conference"
+    if ( time ) {
+        // iterate through the passed-in array
+        for ( let i = 0; i < 3; i++ ) {
+            // if the current element isn't the checked checkbox...
+            if ( time[i] != checkbox ) {
+                // disabled it, or re-enable it depending on whether the checkbox is being checked on unchecked
+                time[i].disabled = checkbox.checked;
+            }
+        }
+    }
+
+    // TOTAL CALCULATOR
+    // if the checkbox.checked == true...
+    if ( checkbox.checked ) {
+        // if the checkbox is the "main conference"...
+        if ( checkbox.name == 'all' ) {
+            // add $200 to the total
+            total += 200;
+        // if the checkbox is any of the other activities...
+        } else {
+            // add $100 to the total
+            total += 100;
+        }
+    // if the checkbox is being unchecked...
+    } else {
+        // if the checkbox is the "main conference"...
+        if ( checkbox.name == 'all' ) {
+            // subtract $200 from the total
+            total -= 200;
+        // if the checkbox is any of the other activites...
+        } else {
+            // subtract $100 from the total
+            total -= 100;
+        }
+    }
+    // update content of the #total element to represent current total
+    totalEl.textContent = `Total: $${total}`;
+}

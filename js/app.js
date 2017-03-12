3d085e5f84a73e8de5401c588b6215e540358be0
Kevin Kenger <kevinkenger@gmail.com>
2017-03-12 13:39:44 -0700

    added submit functionality and error handling

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
-const nameInput = document.getElementById('name'),
+const totalEl = document.createElement('div'),
+      nameInput = document.getElementById('name'),
+      emailInput = document.getElementById('mail'),
+      form = document.getElementsByTagName('form')[0],
       jobTitle = document.getElementById('other-title'),
       basicInfo = document.getElementsByTagName('fieldset')[0],
       tshirtInfo = document.getElementsByClassName('shirt')[0],
-      activitiesInfo = document.getElementsByClassName('activities')[0],
       paymentInfo = document.getElementsByTagName('fieldset')[3],
-      totalEl = document.createElement('div');
+      activitiesInfo = document.getElementsByClassName('activities')[0];
+      
+let total = 0,
+    errors = 0;
 
-let total = 0;
 totalEl.textContent = `Total: $${total}`;
 
 window.onload = () => {    
@@ -14,6 +18,7 @@ window.onload = () => {
     jobTitle.style.display = 'none';
     totalEl.id = 'total';
     activitiesInfo.appendChild(totalEl);
+    tshirtInfo.children[4].style.display = 'none';
 }
 
 
@@ -34,6 +39,7 @@ tshirtInfo.addEventListener('change', (e) => {
     if ( e.target.id != 'design' ) {
         return;
     }
+    tshirtInfo.children[4].style.display = '';
     const colors = document.getElementById('color').children;
     if ( e.target.value == 'js puns' ) {
         for ( let i = 0; i < colors.length; i++ ) {
@@ -133,4 +139,101 @@ paymentInfo.addEventListener('change', (e) => {
             cc.nextElementSibling.nextElementSibling.style.display = '';
             break;
     }
+});
+
+
+paymentInfo.addEventListener('keyup', (e) => {
+    if ( e.target.id == 'cvv' || e.target.id == 'zip' || e.target.id == 'cc-num') {
+        if ( e.code.match(/Key/) ) {
+            e.target.value = e.target.value.replace(/\D/g, '');
+        }
+        if ( e.target.id == 'cvv' ) {
+            if ( e.target.value.length > 3 ) {
+                e.target.value = e.target.value.substring(0, e.target.value.length - 1);
+            } 
+        }
+    }
+});
+
+
+
+const errorOnField = (input, bool) => {
+    const label = input.previousElementSibling;
+    if ( bool ) {
+        label.style.color = 'darkred';
+        label.style.fontWeight = 'bold';
+        label.firstElementChild.style.display = 'inline';
+    } else {
+        label.style.color = 'black';
+        label.style.fontWeight = 'normal';
+        label.firstElementChild.style.display = 'none';
+    }
+}
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+function validateCreditCard(cc) {
+    var re = /^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][‌​0-9])[0-9]{12}|3[47]‌​[0-9]{13}|3(?:0[0-5]‌​|[68][0-9])[0-9]{11}‌​|(?:2131|1800|35\\d{‌​3})\\d{11})$/
+    return re.test(cc);
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let activityChosen,
+        paymentMethodChosen;
+
+    if ( nameInput.value.trim() == '' ) {
+        errorOnField(nameInput, true);
+    } else {
+        errorOnField(nameInput, false);
+    }
+    if ( emailInput.value.trim() == '' || !validateEmail(emailInput.value) ) {
+        errorOnField(emailInput, true);
+    } else {
+        errorOnField(emailInput, false);
+    }
+
+    if ( document.getElementById('design').value != 'js puns' && document.getElementById('design') != 'heart js' ) {
+        tshirtInfo.children[1].style.display = 'block';
+    } else {
+        tshirtInfo.children[1].style.display = 'none';
+    }
+
+    for ( let i = 0; i < activitiesInfo.children.length; i++ ) {
+        if ( activitiesInfo.children[i].tagName == 'LABEL' ) {
+            if ( activitiesInfo.children[i].firstElementChild.checked ) {
+                activityChosen = true;
+            }
+        }
+    }
+
+    if ( activityChosen ) {
+        activitiesInfo.children[1].style.display = 'none';
+    } else {
+        activitiesInfo.children[1].style.display = 'block';
+    }
+
+    if ( document.getElementById('payment').value == 'select_method' ) {
+        paymentInfo.children[1].style.display = 'block';
+    } else {
+        paymentInfo.children[1].style.display = 'none';
+        if ( document.getElementById('payment').value == 'credit card' ) {
+            if ( document.getElementById('cc-num').value == '' || document.getElementById('zip').value == '' || document.getElementById('cvv').value == '' ) {
+                document.getElementById('credit-card').firstElementChild.style.display = 'block';
+            } else {
+                document.getElementById('credit-card').firstElementChild.style.display = 'none';
+            }
+            if ( !validateCreditCard(document.getElementById('cc-num').value) ) {
+                document.getElementById('credit-card').children[1].style.display = 'block';
+            } else {
+                document.getElementById('credit-card').children[1].style.display = 'none';
+            }
+        }
+    }
+
+
 });
